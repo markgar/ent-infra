53b2623a567929607bc8261d9aa644fe571b235c
Mark Garner <mgarner@microsoft.com>
2021-04-08 19:26:32 -0500

    fix location of bicep file

--- rg/aks/deploy.ps1
+++ rg/aks/deploy.ps1
@@ -3,4 +3,4 @@ $rgname = "m-" + $rgname
 
 $deploymentName = Get-Date -Format "yyyyMMddHHmmss"
 
-az deployment sub create --location eastus --template-file main.bicep --name $deploymentName --template-file main.bicep --parameters rgName=$rgname --name $deploymentName
+az deployment sub create --location eastus --template-file ./main.bicep --name $deploymentName --parameters rgName=$rgname --name $deploymentName
