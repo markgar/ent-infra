4e2a22ea7621bc2a1d3cef49a2a17fc30aed8080
Mark Garner <mgarner@microsoft.com>
2021-06-16 11:30:59 -0500

    switch to inline

--- rg/arc-host/deploy.ps1
+++
@@ -1,6 +0,0 @@
-$rgname = (Get-Location).Path.Split("/")[(Get-Location).Path.Split("/").Count-1]
-$rgname = "m-" + $rgname
-
-$deploymentName = Get-Date -Format "yyyyMMddHHmmss"
-
-az deployment sub create --location eastus --template-file ./main.bicep --name $deploymentName --parameters ./main.parameters.json rgName=$rgname --name $deploymentName
