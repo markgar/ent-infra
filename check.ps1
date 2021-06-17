0274a5771ededb0df8c8c83182c5a45a13550d71
Mark Garner <mgarner@microsoft.com>
2021-06-17 15:32:00 -0500

    first run

--- check.ps1
+++ check.ps1
@@ -1,14 +1,22 @@
 
 $editedFiles = @( git diff HEAD HEAD~ --name-only )
-Write-Host "Edited files:"
-Write-Host $editedFiles
+ Write-Host 'Edited files:'
+ Write-Host $editedFiles
 
 $editedFilesFiltered = $editedFiles -match "rg/*"
 
-Get-Unique  -InputObject $editedFilesFiltered
+$directoriesWithChanges = `
+$(foreach ($file in $editedFilesFiltered) {
+    $file.split('/')[1]
+}) | Get-Unique
 
-foreach ($file in $editedFilesFiltered) {
-    
-    Write-Host "Filtered file: $file"
+Write-Host 'RGs to deploy:'
+Write-Host $directoriesWithChanges
 
+foreach ($directory in $directoriesWithChanges) {
+    $dirToCheck = './rg/' + $directory
+    # Get-ChildItem -Path $dirToCheck
+    $commandToExecute = $dirToCheck + '/deploy.ps1'
+    Invoke-Command -FilePath $commandToExecute
 }
+
